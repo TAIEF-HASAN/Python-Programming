c8c9544ecf6cd495339c7b891bbb98dd2e1db463
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-14 16:24:28 +0600

    added new file using Variables

---
+++ Python Basics/Script2.py
@@ -0,0 +1,58 @@
+# Using Variables
+
+status = "Watching Netflix"
+#update
+status = "Relaxing at the beach"
+print(status)
+#we can re-update it again and again
+status = "Incomplete"
+status = "Complete"
+print(status)
+status = "New data required"
+print(status)
+
+default_option = "upload"
+new_status = "download"
+new_status = default_option
+print(new_status)
+
+#when update a variable, it forgets its previous value
+status = "playing football"
+print(status)
+
+status = "walking the dog"
+print(status)
+
+print("Followers:" + "55")
+#same as before
+followers = "55"
+print("Followers:" + followers) 
+
+lebel = "Posts:" + "13"
+print(lebel)
+
+#Sign + Addition, * multiplecation, / divition, - subtruction
+
+#addition
+active_users = 5
+number_of_application = 5+1
+print(number_of_application)
+
+#multiply
+percent = 0.5 * 100
+print(percent)
+
+number_of_steps = 70;
+print("You're on step: ")
+print(number_of_steps+1)
+
+private = 3
+public = 10
+total = public + private
+print("Total posts:")
+print(total)
+
+sum_of_grade = 40;
+student = 5
+print(sum_of_grade/student)
+
