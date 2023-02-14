3549ad6d2c57e072c110e9c845e997fb182b4af8
TAIEF-HASAN <taief.hasan880@gmail.com>
2023-02-14 16:26:04 +0600

    Added new file of Boolean value True and False

---
+++ Python Basics/Script3.py
@@ -0,0 +1,16 @@
+#boolean
+power_on = True;
+print(power_on)
+
+print("Load data")
+status = False
+print(status)
+
+print("Auto update")
+print(False)
+
+#not is the negation operator. it turns values into their opposite.
+print(not True)
+print(not False)
+available = True
+print(not available)
